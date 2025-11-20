c82c586736ab8f3337aec6b0720c38e26a6ac664
jayasankar80651 <jaya80651@gmail.com>
2025-11-20 23:43:27 +0530

    second commite 20/11/25

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:package_getx_using_inputtext/home.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(GetMaterialApp(
       home:GetxUsingTextInput(),
       debugShowCheckedModeBanner: false,
-    );
+  ));
   }
-}
+
      
